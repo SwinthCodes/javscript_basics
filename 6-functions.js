f3c55f96dedd2d0cfda8d38a8aaea8bf8404bbb9
Bryan Warren <bryan.warren77@gmail.com>
2023-03-25 16:35:39 -0500

    completed function sections

--- 6-functions.js
+++ 6-functions.js
@@ -102,7 +102,186 @@
  *
  *  A function can call itself.
  *
- *  
+ *  a function defined in the global scope can access all variables defined in the global 
+ *  scope. A function defined inside another function can also access all variables defined 
+ *  in its parent function, and any other variables to which the parent function has access
+ *
+ * 
+ * 
+ */
+
+// The followirng variables are defined in the global scope
+const num1 = 20;
+const num2 = 3;
+const name = 'Chamahk';
+
+// This function is defined in the global scope
+function multiply() {
+    return num1 * num2;
+}
+
+multiply(); // Returns 60
+
+// A nested function example
+function getScore() {
+    const num1 = 2;
+    const num2 = 3;
+
+    function add() {
+        return name + ' scored ' + (num1 + num2);
+    }
+
+    return add();
+}
+
+getScore(); // Returns "Chamahk scored 5"
+
+/**
+ * Recursion
+ *  A function can refer to and call itself. There are three ways for a function to 
+ *  refer to itself:
+ *     the function's name
+ *     arguments.callee
+ *     an in-scope variable that refers to the function
+ *      
+ * 
  *
  */
 
+function loop(x) {
+    if (x >= 5) // termination condition
+        return;
+    console.log(x); // statement to execute
+    loop(x + 1); // function's call itself
+}
+loop(0);
+
+// A recursive function to traverse a tree of nodes from the OM
+function walkTree(node) {
+    if (node == null)
+        return;
+    console.log(node);
+    for (let i = 0; i < node.childNodes.length; i++) {
+        walkTree(node.childNodes[i]);
+    }
+}
+
+/**
+ *  Closures
+ *  Closures are one of the most powerful features of JavaScript. 
+ *  JavaScript allows for the nesting of functions and grants the inner
+ *  function full access to all the variables and functions defined inside 
+ *  the outer function
+ * 
+ *  The outer function does not have access to the variables and functions defined
+ *  inside the inner function. This provides a sort of encapsulation for the variables 
+ *  of the inner function
+ * 
+ * 
+ */
+
+const pet = function(name) { // The outer function defines a variable called "name"
+    const getName = function() {
+        return name; // The inner function has access to the "name" variable of the outer function
+    }
+    return getName; // Return the inner function, thereby exposing it to outer scopes
+}
+
+const myPet = pet('Vivie');
+console.log(myPet());
+
+const createPet = function(name){
+    let sex;
+    const pet = {
+        setName(newName){
+            name = newName;
+        },
+        getName(){
+            return name;
+        },
+        getSex(){
+            return sex;
+        },
+        setSex(newSex){
+            if(typeof newSex ==='string' &&
+             (newSex.toLocaleLowerCase() === 'male' || newSex.toLocaleLowerCase()=='female')){
+                sex = newSex;
+             }
+        }
+    }
+
+    return pet;
+
+}
+
+const newPet = createPet('Vivian');
+console.log(newPet.getName());
+newPet.setName('Oliver');
+newPet.setSex('Male')
+console.log(newPet.getName());
+console.log(newPet.getSex());
+
+
+/**
+ * The arguments object
+ * 
+ *   The arguments of a function are maintained in an array-like object.
+ *  
+ *  This is often useful if you don't know in advance how many arguments will be passed 
+ *  to the function. You can use arguments.length to determine the number of arguments 
+ *  actually passed to the function, and then access each argument using the arguments 
+ *  object.
+ * 
+ */
+
+function myConcat(separator) {
+    let result = ''; // initialize list
+    let i;
+    // iterate through arguments
+    for (i = 1; i < arguments.length; i++) {
+        result += arguments[i] + separator;
+    }
+    return result;
+}
+
+console.log(myConcat(', ', 'red', 'orange', 'blue')); 
+console.log(myConcat('; ', 'elephant', 'giraffe', 'lion', 'cheetah')); 
+console.log(myConcat('. ', 'sage', 'basil', 'oregano', 'pepper', 'parsley')); 
+
+
+/**
+ * Default and rest parameters
+ * 
+ *  Default parameters allow you to set the value of a parameter if no value is passed
+ *  Rest parameters allow you to pass an arbitrary number of arguments to a function
+ * 
+ */
+
+//default parameters
+function multiplyDefaultParams(a, b = 1) {
+    return a * b;
+}
+
+console.log(multiplyDefaultParams(5));
+
+//rest parameters
+function multiplyRestParams(multiplier, ...theArgs) {
+    return theArgs.map(x => multiplier * x);
+}
+
+const mulArray = multiplyRestParams(2, 1, 2, 3);
+console.log(mulArray);
+
+/**
+ * Arrow functions
+ *  a shorter syntax compared to function expressions and does not have its own this,
+ *  arguments, super, or new.target. Arrow functions are always anonymous.
+ * 
+ * 
+ */
+
+function normalFunc(){
+    return 'Normal Function';
+}
+
+()=> 'Arrow Function';
